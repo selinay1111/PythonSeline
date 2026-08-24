31a0bb5e78131ad084f82f4f272ef748fd7cf30f
Winston Zhu <winston.zhu@yahoo.ca>
2026-08-24 19:58:43 -0400

    completed log puzzle

--- google-python-exercises/google-python-exercises/logpuzzle/logpuzzle.py
+++ google-python-exercises/google-python-exercises/logpuzzle/logpuzzle.py
@@ -8,8 +8,9 @@
 
 import os
 import re
+import shutil
 import sys
-import urllib
+import urllib.request
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
@@ -19,43 +20,65 @@ Here's what a puzzle url looks like:
 """
 
 
-def read_urls(filename):
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
-  
+def read_urls(filename, puzzle):
+    """Returns a list of the puzzle urls from the given log file,
+    extracting the hostname from the filename itself.
+    Screens out duplicate urls and returns the urls sorted into
+    increasing order."""
+    # +++your code here+++
+    if puzzle == "animal":
+        with open(filename) as f:
+            result = []
+            for line in f:
+                match = re.search(r'/edu/languages/google-python-class/images/puzzle/a-\w\w\w\w\.jpg', line)
+                if match:
+                    result.append("https://code.google.com" + match.group())
+        return sorted(list(set(result)))
+    else:
+        with open(filename) as f:
+            result = {}
+            for line in f:
+                match = re.search(r'/edu/languages/google-python-class/images/puzzle/p-\w\w\w\w-(\w\w\w\w)\.jpg', line)
+                if match:
+                    print("match found")
+                    result[match.group(1)] = "https://code.google.com" + match.group()
+            sorted_result = dict(sorted(result.items()))
+            print(sorted_result)
+        return list(sorted_result.values())
+
 
 def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
-  # +++your code here+++
+    """Given the urls already in the correct order, downloads
+    each image into the given directory.
+    Gives the images local filenames img0, img1, and so on.
+    Creates an index.html in the directory
+    with an img tag to show each local image file.
+    Creates the directory if necessary.
+    """
+    # +++your code here+++
+    # delete old folder and make a new one
+    if os.path.exists(dest_dir):
+        print(dest_dir)
+        print("Removing existing output directory...")
+        shutil.rmtree(dest_dir)
+    if not os.path.exists(dest_dir):
+        print(dest_dir)
+        print("Creating output directory...")
+        os.mkdir(dest_dir)
+
+    # download all the images and create html file
+    with open("index.html", "w") as f:
+        f.write("<html>\n<body>\n")
+        count = 1
+        for url in img_urls:
+            urllib.request.urlretrieve(url, f"./animal_images/animal_img_{count}.jpg")
+            f.write(f'<img src="./animal_images/animal_img_{count}.jpg">')
+            count += 1
+        f.write("</body>\n</html>")
 
 
 def main():
-  args = sys.argv[1:]
-
-  if not args:
-    print('usage: [--todir dir] logfile ')
-    sys.exit(1)
-
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
-
-  img_urls = read_urls(args[0])
-
-  if todir:
-    download_images(img_urls, todir)
-  else:
-    print('\n'.join(img_urls))
+    download_images(read_urls("place_code.google.com", "place"), "./animal_images")
 
 if __name__ == '__main__':
-  main()
+    main()
